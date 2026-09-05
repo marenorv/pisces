b9606616d9b7babeac8a9195cff8b700235ae898
marorv <marorv95@gmail.com>
2026-09-05 21:11:39 +0200

    Show organizations for a facility

--- frontend/src/components/facility-details/FacilityDetails.tsx
+++ frontend/src/components/facility-details/FacilityDetails.tsx
@@ -1,27 +1,32 @@
 import type {FC} from "react";
 import {useIntl} from "react-intl";
 import {useQuery} from "@tanstack/react-query";
-import {useNavigate, useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import * as api from "@api";
 import {Loader} from "@components/common/Loader.tsx";
+import {OrganizationsList} from "@components/facility-details/OrganizationsList.tsx";
 
 export const FacilityDetails: FC = () => {
     const formatMessage = useIntl().formatMessage;
-    const navigate = useNavigate();
     const {id} = useParams<{ id: string }>();
 
-    const {data: facility = null} = useQuery({
+    const {data: facility = null, isLoading, isError} = useQuery({
         queryKey: ["facility", id],
         queryFn: () => api.getFacilityById(id!),
         enabled: !!id,
     });
 
-    if (!facility) {
-        return <Loader />
+    if (isLoading) {
+        return <Loader/>
+    }
+
+    if (isError || !facility) {
+        return <p>{formatMessage({id: 'facilityDetails.notFound'})}</p>
     }
 
     return <div>
-        <button onClick={() => navigate('/')}>{formatMessage({id: 'facilityDetails.back'})}</button>
-        <div>{facility.name}</div>
+        <Link className='pisces-back-link' to='/'>{formatMessage({id: 'facilityDetails.back'})}</Link>
+        <h2>{facility.name}</h2>
+        <OrganizationsList organizations={facility.organizations}/>
     </div>
 }
--- frontend/src/types/facilities.ts
+++ frontend/src/types/facilities.ts
@@ -2,4 +2,10 @@ export interface Facility {
     id: string;
     name: string;
     registeredDate: string;
+    organizations: Organization[]
+}
+
+export interface Organization {
+    id: string;
+    name: string;
 }
