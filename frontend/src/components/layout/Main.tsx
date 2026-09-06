265ffdb295f8a662bf3bb131c377425c0e5d5587
marorv <marorv95@gmail.com>
2026-09-06 16:36:30 +0200

    Route / -> /facilities

--- frontend/src/components/layout/Main.tsx
+++ frontend/src/components/layout/Main.tsx
@@ -1,13 +1,16 @@
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import {FacilityList} from "@components/facility-list/FacilityList.tsx";
 import {FacilityDetails} from "@components/facility-details/FacilityDetails.tsx";
 import {EditFacility} from "@components/facility-edit/EditFacility.tsx";
+import {AddFacility} from "@components/add-facility/AddFacility.tsx";
 
 export const Main = () => {
     return (
         <div className='pisces-main'>
             <Routes>
-                <Route path='/' element={<FacilityList/>}/>
+                <Route path='/' element={<Navigate to='/facilities' replace/>}/>
+                <Route path='/facilities' element={<FacilityList/>}/>
+                <Route path='/facilities/:ew' element={<AddFacility/>}/>
                 <Route path='/facilities/:id' element={<FacilityDetails/>}/>
                 <Route path='/facilities/:id/edit' element={<EditFacility/>}/>
             </Routes>
